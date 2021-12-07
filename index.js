a56fa822ec18c1ec2e53a8a76d604af69960b943
Fátima Blanco Muñoz <fatima.blanco@kairosds.com>
2021-12-07 10:54:56 +0100

    add post, get and getById students

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const boton = document.getElementById("prueba");
-boton.addEventListener("click", function (evt) {
+const post = document.getElementById("post");
+const get = document.getElementById("get");
+const getId = document.getElementById("getId");
+const id = "61ae77a466d2ace3bab3039d";
+
+getId.addEventListener("click", function (evt) {
+  console.log("boton");
+  const request = new Request(`http://localhost:8090/students/${id}`, {
+    method: "GET",
+    mode: "no-cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+  console.log("request =", request);
+  fetch(request)
+    .then(function (response) {
+      console.log("response =", response);
+      return response;
+    })
+    .then(function (data) {
+      console.log("data = ", data);
+    })
+    .catch(function (err) {
+      console.error(err, "ERROR GET");
+    });
+});
+
+post.addEventListener("click", function (evt) {
   console.log("click");
   const request = new Request("http://localhost:8090/students", {
     method: "POST",
@@ -30,5 +57,27 @@ boton.addEventListener("click", function (evt) {
     })
     .catch(function (err) {
       console.error(err);
+    });
+});
+
+get.addEventListener("click", function (evt) {
+    const request = new Request("http://localhost:8090/students", {
+    method: "GET",
+    mode: 'no-cors',
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+    },
+    });
+    console.log("request =", request);
+    fetch(request)
+    .then(function (response) {
+        console.log("response =", response);
+        return response.text();
+    })
+    .then(function (data) {
+        console.log("data = ", data);
+    })
+    .catch(function (err) {
+        console.error(err, "ERROR GET");
     });
 });
